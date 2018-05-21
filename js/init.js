1ff6fd64405a2751087172101a0a8b26e396b5bf
Olga Grichanovskaya <olga.grichanovskaya@gmail.com>
2018-05-21 23:39:36 +0300

    add section portfolio

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ $( function() {
     // init tabs
     $( ".services-tab" ).tabs();
     // init portfolio tabs
-
+    $( ".portfolio-tabs" ).tabs();
     //init reviews carousel
     // $('.reviews-carousel').slick({
     //     dots: true,
