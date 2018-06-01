967c858294a4b68bab2521cfb25e841ddf66d046
Olga Grichanovskaya <olga.grichanovskaya@gmail.com>
2018-06-01 16:09:56 +0300

    add animation

--- js/init.js
+++ js/init.js
@@ -9,11 +9,13 @@ $( function() {
         autoplay: true,
         autoplaySpeed: 5000,
         prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
-        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><~i class="fa fa-chevron-right"></i></div></div>'
     });
     //init slicknav
     $('.header-nav').slicknav({
         appendTo: '#header .container-fluid',
         label: ''
     });
+    // Wow init
+    new WOW().init();
 } );
